24eb189e9151b472fd3cc60ba4e768a844d73f2f
AbdullaHossamDev <abdulla.bably99@gmail.com>
2020-06-01 17:31:35 +0200

    update panel module

--- src/app/panel/panel.module.ts
+++ src/app/panel/panel.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelRoutingModule } from './panel-routing.module';
-import { PostControlComponentComponent } from './post-control-component/post-control-component.component';
-import { AllPostsComponentComponent } from './all-posts-component/all-posts-component.component';
 import { PostControlComponent } from './post-control/post-control.component';
 import { AllPostsComponent } from './all-posts/all-posts.component';
 
 
 @NgModule({
-  declarations: [PostControlComponentComponent, AllPostsComponentComponent, PostControlComponent, AllPostsComponent],
+  declarations: [PostControlComponent, AllPostsComponent],
   imports: [
     CommonModule,
     PanelRoutingModule
